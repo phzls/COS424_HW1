4125b4d98eafa235c2248be491d02b4620db15a9
Liangsheng <liangshe@princeton.edu>
2015-02-27 16:24:03 -0500

    Plot ROC and PRC curves for full NB, NB with mutual information and KNN

--- scikit_nb.py
+++ scikit_nb.py
@@ -10,3 +10,10 @@ clf = MultinomialNB().fit(DP.train_bag_words_transformed, DP.train_email_class)
 predicted = clf.predict(DP.test_bag_words_transformed)
 
 pr.test_result(predicted, DP.test_email_class)
+
+proba = clf.predict_proba(DP.test_bag_words_transformed)
+
+filename = "NB_Full_Data_"
+DP.ROC_curve(proba[:,1], plot_show=False, filename=filename + "ROC")
+
+DP.PRC_curve(proba[:,1], filename=filename + "PRC")
